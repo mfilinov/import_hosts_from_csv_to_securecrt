1fedfb31134f31b2ac004d455c928f977db7efcb
mfilinov <mfilinov@mail.ru>
2018-08-12 20:28:30 +0300

    add -ip option

--- create_telnet_ports.py
+++ create_telnet_ports.py
@@ -4,16 +4,17 @@ import csv
 parser = argparse.ArgumentParser(description='generate ports')
 parser.add_argument('-start', action="store", dest="start", type=int)
 parser.add_argument('-stop', action="store", dest="stop", type=int)
+parser.add_argument('-ip', action="store", dest="ip", type=str, default='127.0.0.1')
 parser.add_argument('-f', action="store", dest="folder", type=str, default='')
 args = parser.parse_args()
 header = ['session_name', 'hostname', 'folder', 'protocol', 'port', 'emulation']
-def generate_telnet_ports(start,stop,folder):
+def generate_telnet_ports(start,stop,folder,ip):
     result = []
     for i in range(start, stop + 1):
-        list_result = ['port_' + str(i),'127.0.0.1', folder, 'telnet', i, 'xterm']
+        list_result = ['port_' + str(i), ip, folder, 'telnet', i, 'xterm']
         result.append(list_result)
     return result
-telnet_list = generate_telnet_ports(start=args.start,stop=args.stop,folder=args.folder)
+telnet_list = generate_telnet_ports(start=args.start,stop=args.stop,folder=args.folder,ip=args.ip)
 '''write header'''
 with open('to_securecrt.csv', 'w') as f:
     writer = csv.writer(f, delimiter=',')
--- readme.md
+++ readme.md
@@ -2,7 +2,8 @@ script for generate sessions in securecrt:
 1. turn the button "run script" and select python file ImportArbitraryDataFromFileToSecureCRTSessions
 2. In the open window select your **to_secure.csv** file
 
-generate **to_secure.csv** file for localhost connections:
-1. in command line type "python localhost_telnet.py -start 5001 -stop 5020 -f lab/lab"
+generate **to_secure.csv** file for telnet connections:
+1. in command line type "python localhost_telnet.py -start 5001 -stop 5020 -f lab/lab -ip 10.0.0.1"
 2. range of ports: `-start` port telnet connection, `-stop` port telnet connection
 3. folder for sessions: `-f` _destfolder/destfolder_, default is root directory
+4. ip address for telnet connection: `-ip` _10.0.0.1_, default is 127.0.0.1
