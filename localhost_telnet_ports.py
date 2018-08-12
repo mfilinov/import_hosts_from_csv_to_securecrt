6f8a9ee83077562402c0bad1164357bfb227e167
mfilinov <mfilinov@mail.ru>
2018-08-12 17:26:16 +0300

    first commit

---
+++ localhost_telnet_ports.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import argparse
+import csv
+parser = argparse.ArgumentParser(description='generate ports')
+parser.add_argument('-start', action="store", dest="start", type=int)
+parser.add_argument('-stop', action="store", dest="stop", type=int)
+parser.add_argument('-f', action="store", dest="folder", type=str, default='')
+args = parser.parse_args()
+header = ['session_name', 'hostname', 'folder', 'protocol', 'port', 'emulation']
+def generate_telnet_ports(start,stop,folder):
+    result = []
+    for i in range(start, stop + 1):
+        list_result = ['port_' + str(i),'127.0.0.1', folder, 'telnet', i, 'xterm']
+        result.append(list_result)
+    return result
+telnet_list = generate_telnet_ports(start=args.start,stop=args.stop,folder=args.folder)
+'''write header'''
+with open('to_securecrt.csv', 'w') as f:
+    writer = csv.writer(f, delimiter=',')
+    writer.writerow(header)
+'''append sessions'''
+with open('to_securecrt.csv', 'a') as f:
+    writer = csv.writer(f, delimiter=',')
+    for row in telnet_list:
+        writer.writerow(row)
--- readme.md
+++ readme.md
@@ -1,6 +1,8 @@
 script for generate sessions in securecrt:
 1. turn the button "run script" and select python file ImportArbitraryDataFromFileToSecureCRTSessions
-2. In the open window select your .csv file
+2. In the open window select your **to_secure.csv** file
 
-generate csv file:
-1. localhost_telnet.py
+generate **to_secure.csv** file for localhost connections:
+1. in command line type "python localhost_telnet.py -start 5001 -stop 5020 -f lab/lab"
+2. range of ports: `-start` port telnet connection, `-stop` port telnet connection
+3. folder for sessions: `-f` _destfolder/destfolder_, default is root directory
